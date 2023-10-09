2f6e04702a21cfa173e188f5f254c8ad949284d0
Rishav Sharma <rishavtosharma@gmail.com>
2023-10-09 23:03:01 +0530

    feat: Responsivess-1

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,10 +1,15 @@
-import React from 'react'
+
+"use client"
+import React, { useState } from 'react'
 import styles from "./navbar.module.css"
 import Image from 'next/image'
 import Link from 'next/link'
 import ThemeToggle from '../themeToggle/ThemeToggle'
 import AuthLinks from '../authLinks/AuthLinks'
+import { RxHamburgerMenu } from "react-icons/rx"
+import { AiOutlineClose } from "react-icons/ai"
 const Navbar = () => {
+  const [open, setOpen] = useState(false)
   return (
     <div className={styles.container}>
       <div className={styles.social}>
@@ -13,13 +18,15 @@ const Navbar = () => {
         <Image src="/tiktok.png" alt="linkedin logo" width={24} height={24} />
         <Image src="/youtube.png" alt="youtube logo" width={24} height={24} />
       </div>
-      <div className={styles.logo}>Rishav Sharma - Blog</div>
+      <div className={styles.logo}>Rishav Sharma</div>
       <div className={`${styles.links} justify-end`}>
         <ThemeToggle />
-        <Link href="/">Home</Link>
-        <Link href="/contact">Contact</Link>
-        <Link href="/about">About</Link>
+        <Link className={styles.link} href="/">Home</Link>
+        <Link className={styles.link} href="/contact">Contact</Link>
+        <Link className={styles.link} href="/about">About</Link>
         <AuthLinks />
+        <RxHamburgerMenu onClick={() => setOpen(true)} className={`${open ? "hidden" : "block"} sm:hidden`} />
+        <AiOutlineClose onClick={() => setOpen(false)} className={`${!open && "hidden"}`} />
       </div>
     </div>
   )
