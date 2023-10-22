ed93064c1291c3fe4d5b7bf2821f048435099607
Rishav Sharma <rishavtosharma@gmail.com>
2023-10-22 06:01:02 +0530

    ci: responsive navlinks-1

--- src/components/authLinks/AuthLinks.jsx
+++ src/components/authLinks/AuthLinks.jsx
@@ -2,6 +2,8 @@ import { useState } from "react";
 import styles from "./authLinks.module.css";
 import Link from "next/link";
 import { RxHamburgerMenu } from "react-icons/rx";
+import { AiOutlineCloseCircle } from "react-icons/ai";
+import { cn } from "@/utils";
 
 const AuthLinks = () => {
   const [open, setOpen] = useState(false)
@@ -17,22 +19,26 @@ const AuthLinks = () => {
           </>)
       }
       <RxHamburgerMenu onClick={() => setOpen(true)} className={`${open ? "hidden" : "block"} sm:hidden cursor-pointer`} />
+
       {open &&
-        <div className='items-center w-screen h-screen bg-red-600 lex '>
-          <div className="absolute flex flex-col w-full h-full gap-20 left-2">
-            <Link className="" href="/">Home</Link>
-            <Link className="" href="/contact">Contact</Link>
-            <Link className="" href="/about">About</Link>
-            {
-              status === "notauthenticated" ?
-                (<Link href="/login">Login</Link>) :
-                (<>
-                  <Link href="/write">Write</Link>
-                  <span className={styles.link}>Logout</span>
-                </>)
-            }
+        <>                       
+          <AiOutlineCloseCircle style={{width: "1.8rem"}} onClick={() => setOpen(false)} className={cn("z-10 h-5 absolute right-5 top-5 cursor-pointer")} />
+          <div className='absolute bottom-0 right-0 items-center w-screen h-screen pl-8 bg-red-600 left-10'>
+            <div className="relative flex flex-col w-full h-full gap-20 mt-28 ">
+              <Link className="" href="/">Home</Link>
+              <Link className="" href="/contact">Contact</Link>
+              <Link className="" href="/about">About</Link>
+              {
+                status === "notauthenticated" ?
+                  (<Link href="/login">Login</Link>) :
+                  (<>
+                    <Link href="/write">Write</Link>
+                    <span className={styles.link}>Logout</span>
+                  </>)
+              }
+            </div>
           </div>
-        </div>
+        </>
       }
     </>
   )
--- src/components/providers/ThemeProvider.jsx
+++ src/components/providers/ThemeProvider.jsx
@@ -1,3 +1,6 @@
+// We could have supplied the themeContext to any child in the Layout.js file but then we 
+// would have to make the layout file a client component to use contextAPI but we don't want to 
+// do that because it is a server component and we are using metadata in the layout file.
 "use client"
 import { ThemeContext } from '@/context/ThemeContext'
 import { useContext, useEffect, useState } from 'react'
@@ -6,6 +9,9 @@ const ThemeProvider = ({ children }) => {
   const [mounted, setMounted] = useState(false)
   const { theme } = useContext(ThemeContext)
 
+  // We are using this useEffect as some browsers might face issues related to local storage
+  // So, we are checking if the component is mounted first then only we are rendering the whole
+  // application on the web page
   useEffect(() => {
     setMounted(true)
   }, [])
