ead38052a0bc0068c5f02ae74200f42f9ad689d0
Rishav Sharma <rishavtosharma@gmail.com>
2023-12-24 23:57:57 +0530

    ci: added category API route and dynamic category list

--- src/components/categoryList/CategoryList.jsx
+++ src/components/categoryList/CategoryList.jsx
@@ -1,11 +1,50 @@
-import React from "react";
+"use client";
+
+import React, { useEffect, useState } from "react";
 import styles from "./categoryList.module.css";
 import Image from "next/image";
 import Link from "next/link";
 import { cn } from "@/utils/utils";
 
-const categories = [{}];
 const CategoryList = () => {
+  const [categories, setCategories] = useState([]);
+
+  useEffect(() => {
+    const getCategories = async () => {
+      const res = await fetch("http://localhost:3000/api/categories");
+      if (!res.ok) throw new Error("Something went wrong");
+      const data = await res.json();
+      setCategories(data);
+    };
+    getCategories();
+  }, []);
+  const categoriesColors = [
+    "bg-blue-400/50",
+    "bg-pink-400/50",
+    "bg-green-400/50",
+    "bg-red-400/50",
+    "bg-orange-400/50",
+    "bg-purple-400/50",
+  ];
+
+  // If we've used all colors, reset the colors array
+  {
+    /* // Create a copy of the categoriesColors array */
+  }
+  let colors = [...categoriesColors];
+  if (colors?.length === 0) {
+    colors = [...categoriesColors];
+  }
+
+  // Get a random index
+  const randomIndex = Math.floor(Math.random() * colors?.length);
+
+  // Get the color at the random index
+  const color = colors[randomIndex];
+
+  // Remove the color from the array
+  colors.splice(randomIndex, 1);
+
   return (
     <div className="flex flex-col">
       <h1 className="my-12 text-2xl font-bold lg:text-3xl">
@@ -13,90 +52,28 @@ const CategoryList = () => {
       </h1>
       {/* <div className="flex w-full flex-wrap justify-between gap-3"> */}
       <div className={cn(styles.categories, "gap-3 text-[var(--textColor)]")}>
-        <Link
-          href="/blog?cat=style"
-          //   className=" flex h-16 w-full items-center justify-center gap-3 rounded-lg bg-[#57c4ff31] capitalize sm:w-[25%] lg:w-[20%] xl:w-[15%]"
-          className="flex h-16 w-full items-center justify-center gap-3 rounded-lg bg-blue-400/50 capitalize"
-        >
-          <Image
-            className="h-6 w-6 rounded-full"
-            src="/style.png"
-            alt=""
-            height={32}
-            width={32}
-          />
-          <p>Style</p>
-        </Link>
-        <Link
-          href="/blog?cat=style"
-          //   className=" flex h-16 w-full items-center justify-center gap-3 rounded-lg bg-[#57c4ff31] capitalize sm:w-[25%] lg:w-[20%] xl:w-[15%]"
-          className="flex h-16 w-full items-center justify-center gap-3 rounded-lg bg-pink-400/50 capitalize"
-        >
-          <Image
-            className="h-6 w-6 rounded-full"
-            src="/style.png"
-            alt=""
-            height={32}
-            width={32}
-          />
-          <p>Fashion</p>
-        </Link>
-        <Link
-          href="/blog?cat=style"
-          //   className=" flex h-16 w-full items-center justify-center gap-3 rounded-lg bg-[#57c4ff31] capitalize sm:w-[25%] lg:w-[20%] xl:w-[15%]"
-          className="flex h-16 w-full items-center justify-center gap-3 rounded-lg bg-green-400/50 capitalize"
-        >
-          <Image
-            className="h-6 w-6 rounded-full"
-            src="/style.png"
-            alt=""
-            height={32}
-            width={32}
-          />
-          <p>Food</p>
-        </Link>
-        <Link
-          href="/blog?cat=style"
-          //   className=" flex h-16 w-full items-center justify-center gap-3 rounded-lg bg-[#57c4ff31] capitalize sm:w-[25%] lg:w-[20%] xl:w-[15%]"
-          className="flex h-16 w-full items-center justify-center gap-3 rounded-lg bg-red-400/50 capitalize"
-        >
-          <Image
-            className="h-6 w-6 rounded-full"
-            src="/style.png"
-            alt=""
-            height={32}
-            width={32}
-          />
-          <p>Travel</p>
-        </Link>
-        <Link
-          href="/blog?cat=style"
-          //   className=" flex h-16 w-full items-center justify-center gap-3 rounded-lg bg-[#57c4ff31] capitalize sm:w-[25%] lg:w-[20%] xl:w-[15%]"
-          className="flex h-16 w-full items-center justify-center gap-3 rounded-lg bg-orange-400/50 capitalize"
-        >
-          <Image
-            className="h-6 w-6 rounded-full"
-            src="/style.png"
-            alt=""
-            height={32}
-            width={32}
-          />
-          <p>Culture</p>
-        </Link>
-        <Link
-          href="/blog?cat=style"
-          //   className=" flex h-16 w-full items-center justify-center gap-3 rounded-lg bg-[#57c4ff31] capitalize sm:w-[25%] lg:w-[20%] xl:w-[15%]"
-          className="flex h-16 w-full items-center justify-center gap-3 rounded-lg bg-purple-400/50 capitalize"
-        >
-          <Image
-            className="h-6 w-6 rounded-full"
-            src="/style.png"
-            alt=""
-            height={32}
-            width={32}
-          />
-          <p>Coding</p>
-        </Link>
+        {categories?.map((category, index) => {
+          // Use the color in your component
+          return (
+            <Link
+              key={index}
+              href={`/blog?cat=${category.title}`}
+              className={cn(
+                "flex h-16 w-full items-center justify-center gap-3 rounded-lg capitalize",
+                color,
+              )}
+            >
+              <Image
+                className="h-6 w-6 rounded-full"
+                src="/style.png"
+                alt=""
+                height={32}
+                width={32}
+              />
+              <p>{category.title}</p>
+            </Link>
+          );
+        })}
       </div>
     </div>
   );
