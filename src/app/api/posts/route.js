8cca8c536e565f0c018c851263a4245db3e59969
Rishav Sharma <rishavtosharma@gmail.com>
2024-01-04 21:47:47 +0530

    ci: refactor pagination logic in GET /posts route

--- src/app/api/posts/route.js
+++ src/app/api/posts/route.js
@@ -3,16 +3,18 @@ import { NextResponse } from "next/server";
 
 export const GET = async (req) => {
   const { searchParams } = new URL(req.url);
-  const pageNumber = searchParams.get("page") || 1;
+
+  const pageNumber = searchParams.get("page");
   const POST_PER_PAGE = 5;
+
   try {
-    const posts = await prisma.category.findMany({
-      skip: (pageNumber - 1) * POST_PER_PAGE,
+    const posts = await prisma.post.findMany({
       take: POST_PER_PAGE,
+      skip: POST_PER_PAGE * (pageNumber - 1), // this is going to skip the first number of pages that come up after this calculation
     });
     return new NextResponse(JSON.stringify(posts, { status: 200 }));
   } catch (err) {
-    console.log("Error in GET /categories", err);
+    console.log("Error in GET /posts", err);
     return new NextResponse(JSON.stringify({ error: "Something went wrong" }), {
       status: 500,
     });
