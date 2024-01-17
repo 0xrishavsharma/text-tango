d7d2bfdf13a89c5c8e2d26d71e8b095d44547a17
Rishav Sharma <rishavtosharma@gmail.com>
2024-01-17 23:36:42 +0530

    ci: added Firebase storage functionality to upload files

--- src/app/write/page.jsx
+++ src/app/write/page.jsx
@@ -1,6 +1,6 @@
 "use client";
 import Image from "next/image";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "react-quill/dist/quill.bubble.css";
 import { cn } from "@/utils/utils";
 import { AnimatePresence, motion } from "framer-motion";
@@ -11,6 +11,12 @@ const ReactQuill = dynamic(() => import("react-quill"), { ssr: false });
 import { ImImage } from "react-icons/im";
 import { LiaFileUploadSolid } from "react-icons/lia";
 import { RiVideoUploadLine } from "react-icons/ri";
+import {
+  getStorage,
+  ref,
+  uploadBytesResumable,
+  getDownloadURL,
+} from "firebase/storage";
 
 const WritePage = () => {
   const [isOpen, setIsOpen] = useState();
@@ -22,6 +28,41 @@ const WritePage = () => {
     body: "",
     tagList: [],
   });
+
+  useEffect(() => {
+    const storage = getStorage(app);
+    const upload = () => {
+      const name = new Date().getTime() + file.name;
+      const storageRef = ref(storage, name);
+
+      const uploadTask = uploadBytesResumable(storageRef, file);
+
+      uploadTask.on(
+        "state_changed",
+        (snapshot) => {
+          const progress =
+            (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+          console.log("Upload is " + progress + "% done");
+          switch (snapshot.state) {
+            case "paused":
+              console.log("Upload is paused");
+              break;
+            case "running":
+              console.log("Upload is running");
+              break;
+          }
+        },
+        (error) => {},
+        () => {
+          getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+            setMedia(downloadURL);
+          });
+        },
+      );
+    };
+
+    file && upload();
+  }, [file]);
 
   const { status } = useSession();
   const { push } = useRouter();
---
+++ src/utils/firebase.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: "rishavsharmablog.firebaseapp.com",
+  projectId: "rishavsharmablog",
+  storageBucket: "rishavsharmablog.appspot.com",
+  messagingSenderId: "111735041313",
+  appId: "1:111735041313:web:b0f4c7c01e8256213a6260",
+  measurementId: "G-30TML58RPW",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
