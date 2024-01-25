41ad69dba73497749c92de7db68c2257702f140c
Rishav Sharma <rishavtosharma@gmail.com>
2024-01-25 08:05:59 +0530

    refactor: :building_construction: started shifting the project to use TypeScript

---
+++ src/app/api/comments/route.ts
@@ -0,0 +1,52 @@
+import { getAuthSession } from "@/utils/auth";
+import prisma from "@/utils/connect";
+import { NextResponse } from "next/server";
+import { NextApiRequest } from "next";
+
+interface Comment {
+  id: number;
+  content: string;
+  postSlug: string;
+  author: Author;
+}
+
+interface Author {
+  id: number;
+  name: string;
+  email: string;
+}
+
+/**
+ * GET ALL COMMENTS OF A PARTICULAR POST
+ */
+export const POST = async (req: any): Promise<NextResponse> => {
+  const session = await getAuthSession();
+  if (!session)
+    return new NextResponse(
+      JSON.stringify({ message: "You need to be logged in to comment!" }),
+      { status: 401 },
+    );
+
+  const body: Comment = await req.json();
+
+  if (!body.content)
+    return new NextResponse(
+      JSON.stringify({ message: "Comment content is required!" }),
+      { status: 400 },
+    );
+
+  try {
+    const comment: Comment = await prisma.comment.create({
+      data: { ...body, authorEmail: (session as any)?.user.email },
+      include: { author: true },
+    });
+
+    return new NextResponse(JSON.stringify(comment), { status: 201 });
+  } catch (err) {
+    console.log(err);
+    return new NextResponse(
+      JSON.stringify({ message: "Something went wrong!" }),
+      { status: 500 },
+    );
+  }
+};
--- src/components/featured/Featured.jsx
+++ src/components/featured/Featured.jsx
@@ -26,16 +26,14 @@ const Featured = async ({ params }) => {
         {/* <div className='flex gap-8 mt-12 space-between'> */}
         <div className="relative h-64 w-64 flex-1 md:h-96 md:w-96">
           <Image
-            src="/p1.jpeg"
+            src={post?.img}
             className="absolute bottom-0 left-0 right-0 top-0 h-full w-full object-cover"
             fill
             alt=""
           />
         </div>
         <div className="flex flex-1 flex-col justify-center gap-4">
-          <h1 className=" text-3xl font-bold lg:text-4xl">
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit
-          </h1>
+          <h1 className=" text-3xl font-bold lg:text-4xl">{post?.title}</h1>
           <p className="text-base font-light text-[var(--softTextColor)] xl:text-lg">
             <div
               dangerouslySetInnerHTML={{
@@ -45,7 +43,7 @@ const Featured = async ({ params }) => {
             />
           </p>
           <Link
-            href="/"
+            href={`/posts/${post?.slug}`}
             className="w-max rounded bg-[var(--textColor)] px-3 py-2 text-sm text-[var(--bg)] lg:text-base xl:text-lg"
           >
             Read More
