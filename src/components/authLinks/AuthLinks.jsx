35c79b0a04406fcdf7d97c42f6b88a57f735b330
Rishav Sharma <rishavtosharma@gmail.com>
2024-02-17 14:22:32 +0530

    ci: removed nav hamburger, fixed profile photo size and logout button

--- src/components/authLinks/AuthLinks.jsx
+++ src/components/authLinks/AuthLinks.jsx
@@ -46,16 +46,23 @@ const AuthLinks = () => {
           <span
             className={cn(
               styles.link,
-              "flex w-fit cursor-pointer select-none items-center justify-center gap-2 rounded-lg border-black bg-black/5 dark:border-white/30 dark:bg-white/10 sm:relative sm:border-[0.1px] sm:px-3 sm:py-2",
+              "flex w-fit cursor-pointer select-none items-center justify-center gap-2 rounded-full rounded-lg border-black bg-black/5 dark:border-white/30 dark:bg-white/10 sm:relative sm:border-[0.1px] sm:px-3 sm:py-2",
             )}
             onClick={() => setSettingsOpen(!settingsOpen)}
           >
             <div className="rounded-full sm:border-[0.4px] sm:dark:border-white/40">
               <Image
                 src={data?.user.image}
-                className="rounded-full"
+                className="hidden rounded-full sm:block"
                 width={30}
                 height={30}
+                alt="user image"
+              />
+              <Image
+                src={data?.user.image}
+                className=" rounded-full sm:hidden"
+                width={40}
+                height={40}
                 alt="user image"
               />
             </div>
@@ -63,8 +70,8 @@ const AuthLinks = () => {
               {getFirstName(data?.user?.name)}
             </p>
             {settingsOpen && (
-              <div className="absolute left-0 right-0 top-14 z-[9999] m-auto max-h-max w-[90%] items-center justify-center rounded-lg border-[0.1px] border-white/70  bg-black/10 text-white backdrop-blur-sm sm:w-full ">
-                <div className="relative flex w-full flex-col items-center gap-8 rounded-lg bg-white/30 px-6 py-3 text-white backdrop:blur-xl">
+              <div className="absolute right-10 top-20 z-[9999] m-auto max-h-max max-w-max items-center justify-center rounded-lg border-[0.1px] border-white/70 bg-black/10 text-white backdrop-blur-sm sm:left-0 sm:right-0 sm:top-14 sm:w-full ">
+                <div className="relative flex w-full flex-col items-center gap-8 rounded-lg bg-white/30 px-5 py-2 text-white backdrop:blur-xl sm:px-6 sm:py-3">
                   {/* <Link className="" href="/settings">
                     Settings
                   </Link> */}
@@ -77,21 +84,18 @@ const AuthLinks = () => {
           </span>
         </>
       )}
-      {/* <RxHamburgerMenu onClick={() => setOpen(!open)} className={`${open ? "hidden" : "block"} sm:hidden cursor-pointer`} /> */}
-      <RxHamburgerMenu
+
+      {/* <RxHamburgerMenu
         className="cursor-pointer text-2xl md:hidden"
         onClick={() => setOpen(!open)}
-      />
+      /> */}
 
       {open && (
         <>
-          {/* <AiOutlineCloseCircle style={{ width: "1.8rem" }} onClick={() => setOpen(false)} className={cn("z-10 h-5 absolute right-5 top-5 cursor-pointer", !open ? "hidden" : "block")} /> */}
           <m.div
             className="fixed right-0 top-[100px] z-[9999] h-[calc(100vh_-_100px)] w-screen items-center justify-center bg-[var(--bg)]"
             variants={motionNav}
           >
-            {/* <div className={cn('items-center w-screen h-screen pl-8 transition-all bg-red-100 shadow-sm backdrop:blur-3xl left-10', */}
-            {/* open ? "absolute top-0 bottom-0 -right-[1000vw] transition-all" : "hidden")}> */}
             <div className="relative flex h-full w-full select-none flex-col items-center gap-20 pt-36">
               <Link
                 className="text-3xl"
@@ -99,20 +103,6 @@ const AuthLinks = () => {
                 onClick={() => setOpen(false)}
               >
                 Home
-              </Link>
-              <Link
-                className="text-3xl"
-                href="/contact"
-                onClick={() => setOpen(false)}
-              >
-                Contact
-              </Link>
-              <Link
-                className="text-3xl"
-                href="/about"
-                onClick={() => setOpen(false)}
-              >
-                About
               </Link>
               {status === "unauthenticated" ? (
                 <Link
@@ -131,10 +121,6 @@ const AuthLinks = () => {
                   >
                     Write
                   </Link>
-                  {/* <span className={cn(styles.link, "text-4xl")}>
-                    <Image src={data?.user?.user?.image} alt="user image" />
-                    {getFirstName(data?.user?.name)}
-                  </span> */}
                 </>
               )}
             </div>
